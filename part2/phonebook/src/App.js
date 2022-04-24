d01141173b24fc9dd999e8eef4e8f96a9fd8d8ea
nichejambinn <nichejambinn@gmail.com>
2022-04-24 00:21:34 -0400

    Exercise 2.9: The Phonebook Step4

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -5,13 +5,14 @@ const Person = ({ person }) =>
 
 const App = () => {
   const [persons, setPersons] = useState([
-    { 
-      name: "Arto Hellas",
-      number: "040-1234567"
-    }
-  ]) 
+    { name: 'Arto Hellas', number: '040-123456', id: 1 },
+    { name: 'Ada Lovelace', number: '39-44-5323523', id: 2 },
+    { name: 'Dan Abramov', number: '12-43-234345', id: 3 },
+    { name: 'Mary Poppendieck', number: '39-23-6423122', id: 4 }
+  ])
   const [newName, setNewName] = useState("")
   const [newNumber, setNewNumber] = useState("")
+  const [nameFilter, setNameFilter] = useState("")
 
   const addPerson = (event) => {
     event.preventDefault()
@@ -19,7 +20,8 @@ const App = () => {
     if (!persons.some((person) => person.name === newName)) {
       const personObject = {
         name: newName,
-        number: newNumber
+        number: newNumber,
+        id: persons.length + 1
       }
   
       setPersons(persons.concat(personObject))
@@ -28,7 +30,6 @@ const App = () => {
     } else {
       alert(`${newName} is already added to phonebook`)
     }
-    
   }
 
   const handleNameChange = (event) => {
@@ -39,9 +40,19 @@ const App = () => {
     setNewNumber(event.target.value)
   }
 
+  const handleNameFilterChange = (event) => {
+    setNameFilter(event.target.value)
+  }
+
+  const personsToShow = persons.filter(person => person.name.toLowerCase().includes(nameFilter.toLowerCase()))
+
   return (
     <div>
       <h2>Phonebook</h2>
+      <div>
+        filter shown with <input value={nameFilter} onChange={handleNameFilterChange} />
+      </div>
+      <h2>add a new</h2>
       <form onSubmit={addPerson}>
         <div>
           name: <input value={newName} onChange={handleNameChange} />
@@ -55,8 +66,8 @@ const App = () => {
       </form>
       <h2>Numbers</h2>
       <div>
-        {persons.map(person =>
-          <Person key={person.name} person={person} />
+        {personsToShow.map(person =>
+          <Person key={person.id} person={person} />
         )}
       </div>
     </div>
