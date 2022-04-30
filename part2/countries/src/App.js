f201d1b75c24dd3216317543d0a4951676f8f381
nichejambinn <nichejambinn@gmail.com>
2022-04-30 17:31:27 -0400

    Exercise 2.12: Data for countries, step1

---
+++ part2/countries/src/App.js
@@ -0,0 +1,69 @@
+import { useState, useEffect } from 'react'
+import axios from 'axios'
+
+const Country = ({ country }) => 
+  <div>
+    {country.name.common}
+  </div>
+
+const CountryDetails = ({ country }) =>
+  <div>
+    <h1>{country.name.common}</h1>
+    <div>
+      capital {country.capital}<br />
+      area {country.area}
+    </div>
+    <h3>languages:</h3>
+    <ul>
+      {Object.values(country.languages).map(language =>
+        <li>
+          {language}
+        </li>
+      )}
+    </ul>
+    <img src={country.flags.png} alt="country flag" width={150} />
+  </div>
+
+const Countries = ({ countries }) =>
+  <div>
+    {countries.length > 10
+      ? "Too many matches, specify another filter"
+      : countries.length === 1
+        ? <CountryDetails country={countries[0]} />
+        : countries.map(country => 
+            <Country key={country.name.common} country={country} />
+          )
+    }
+  </div>
+
+const App = () => {
+  const [countries, setCountries] = useState([])
+  const [countryFilter, setCountryFilter] = useState("")
+
+  // get all countries
+  useEffect(() => {
+    console.log('effect')
+    axios.get('https://restcountries.com/v3.1/all').then(response => {
+      console.log(`response contains ${response.data.length} records`)
+      setCountries(response.data)
+    })
+  }, [])
+
+  const handleCountryFilterChange = (event) => {
+    console.log(`country filter: ${event.target.value}`)
+    setCountryFilter(event.target.value)
+  }
+
+  const countriesFiltered = countryFilter === "" 
+    ? []
+    : countries.filter(country => country.name.common.toLowerCase().includes(countryFilter.trim().toLowerCase()))
+
+  return (
+    <div>
+      find countries <input value={countryFilter} onChange={handleCountryFilterChange} />
+      <Countries countries={countriesFiltered} />
+    </div>
+  )
+}
+
+export default App;
